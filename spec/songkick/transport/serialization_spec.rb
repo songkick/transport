4a91b70c19fda9fbd047855074c7b9a7ca674472
Joseph Wilk <joe@josephwilk.net>
2012-07-19 15:39:32 +0100

    Don't rely on ordering of hash to string mapping for test.

--- spec/songkick/transport/serialization_spec.rb
+++ spec/songkick/transport/serialization_spec.rb
@@ -10,10 +10,10 @@ describe Songkick::Transport::Serialization do
   end
   
   it "shouldn't scrub passwordy param elements by default" do
-    subject.build_query_string({:foo => "bar", :password => "lol"}).should == "foo=bar&password=lol"
+    subject.build_query_string({:foo => "bar", :password => "lol"}).should include("password=lol")
   end
   
   it "should scrub passwordy param elements when asked" do
-    subject.build_query_string({:foo => "bar", :password => "lol"}, true, true).should == "foo=bar&password=******"
+    subject.build_query_string({:foo => "bar", :password => "lol"}, true, true).should include("password=******")
   end
 end
