9bfe4beb16d89f6e0160a4c647e1df26c3c5b9ee
Joseph Wilk <joe@josephwilk.net>
2012-07-24 11:34:18 +0100

    Tests where rely on a hash ordering which means they break when run under different rubies. Make the tests agnostic of the order of params.

--- spec/songkick/transport/request_spec.rb
+++ spec/songkick/transport/request_spec.rb
@@ -10,19 +10,37 @@ describe Songkick::Transport::Request do
   end
   
   describe :to_s do
-    it "returns the request as a curl command" do
-      get_request.to_s.should == "GET 'www.example.com/?username=Louis&password=CK&access[token]=foo'"
-      post_request.to_s.should == "POST 'www.example.com/' -H 'Content-Type: application/x-www-form-urlencoded' -d 'username=Louis&password=CK&access[token]=foo'"
+    context "with a get request" do
+      it "returns the request as a curl command" do
+        ["GET 'www.example.com/?", "username=Louis", "password=CK", "access[token]=foo"].each{|string| get_request.to_s.should include(string)}
+      end
+    end
+    context "with a post request" do
+      it "returns the request as a curl command" do
+        ["POST 'www.example.com/' -H 'Content-Type: application/x-www-form-urlencoded' -d '",
+          "username=Louis",
+          "password=CK",
+          "access[token]=foo"].each{|string| post_request.to_s.should include(string) }
+      end
     end
     
     describe "with query sanitization" do
       before do
         Songkick::Transport.stub(:sanitized_params).and_return [/password/, "access[token]"]
       end
-      
-      it "removes the parameter values from the request" do
-        get_request.to_s.should == "GET 'www.example.com/?username=Louis&password=[REMOVED]&access[token]=[REMOVED]'"
-        post_request.to_s.should == "POST 'www.example.com/' -H 'Content-Type: application/x-www-form-urlencoded' -d 'username=Louis&password=[REMOVED]&access[token]=[REMOVED]'"
+
+      context "with a get request" do
+        it "removes the parameter values from the request" do
+          ["GET 'www.example.com/?", "username=Louis", "password=[REMOVED]", "access[token]=[REMOVED]"].each{|string| get_request.to_s.should include(string)}
+        end
+      end
+      context "with a post request" do
+        it "removes the parameter values from the request" do
+          ["POST 'www.example.com/' -H 'Content-Type: application/x-www-form-urlencoded' -d '",
+           "username=Louis",
+           "password=[REMOVED]",
+           "access[token]=[REMOVED]"].each{|string| post_request.to_s.should include(string)}
+        end
       end
     end
   end
