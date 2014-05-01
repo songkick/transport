aedc5e50d246ede0b90869df7309cf8dff5a4e31
Daniel Lucraft <dan@fluentradical.com>
2014-05-01 16:53:18 +0100

    Restrict the scope of the meta-programming to make debugging easier

--- lib/songkick/transport/base.rb
+++ lib/songkick/transport/base.rb
@@ -7,21 +7,24 @@ module Songkick
       HTTP_VERBS.each do |verb|
         class_eval %{
           def #{verb}(path, params = {}, head = {}, timeout = nil)
-            req = Request.new(endpoint, '#{verb}', path, params, headers.merge(head), timeout)
-            Reporting.log_request(req)
-
-            req.response = execute_request(req)
-
-            Reporting.log_response(req)
-            Reporting.record(req)
-            req.response
-
-          rescue => error
-            req.error = error
-            Reporting.record(req)
-            raise error
+            do_verb("#{verb}", path, params, head, timeout)
           end
         }
+      end
+
+      def do_verb(verb, path, params = {}, head = {}, timeout = nil)
+        req = Request.new(endpoint, verb, path, params, headers.merge(head), timeout)
+        Reporting.log_request(req)
+
+        req.response = execute_request(req)
+
+        Reporting.log_response(req)
+        Reporting.record(req)
+        req.response
+      rescue => error
+        req.error = error
+        Reporting.record(req)
+        raise error
       end
 
       def with_headers(headers = {})
@@ -32,7 +35,7 @@ module Songkick
         TimeoutDecorator.new(self, timeout)
       end
 
-    private
+      private
 
       def process(url, status, headers, body)
         Response.process(url, status, headers, body)
