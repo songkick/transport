285924850ca5485a621f4190acf72f3e00b879cf
Daniel Lucraft <dan@fluentradical.com>
2014-05-01 16:48:02 +0100

    Remove whitespace

--- lib/songkick/transport/base.rb
+++ lib/songkick/transport/base.rb
@@ -1,21 +1,21 @@
 module Songkick
   module Transport
-    
+
     class Base
       attr_accessor :user_agent
-      
+
       HTTP_VERBS.each do |verb|
         class_eval %{
           def #{verb}(path, params = {}, head = {}, timeout = nil)
             req = Request.new(endpoint, '#{verb}', path, params, headers.merge(head), timeout)
             Reporting.log_request(req)
-            
+
             req.response = execute_request(req)
-            
+
             Reporting.log_response(req)
             Reporting.record(req)
             req.response
-            
+
           rescue => error
             req.error = error
             Reporting.record(req)
@@ -23,33 +23,33 @@ module Songkick
           end
         }
       end
-      
+
       def with_headers(headers = {})
         HeaderDecorator.new(self, headers)
       end
-      
+
       def with_timeout(timeout = DEFAULT_TIMEOUT)
         TimeoutDecorator.new(self, timeout)
       end
-      
+
     private
-      
+
       def process(url, status, headers, body)
         Response.process(url, status, headers, body)
       end
-      
+
       def headers
         Headers.new(
           'Connection' => 'close',
           'User-Agent' => user_agent
         )
       end
-      
+
       def logger
         Transport.logger
       end
     end
-    
+
   end
 end
 
