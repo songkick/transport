07cb2dab3897e5a127361f8b7157a174b1f34518
Robin Tweedie <robin@songkick.com>
2015-06-17 15:39:02 +0100

    use plain RSpec instead of FakeCurl

--- spec/songkick/transport/curb_spec.rb
+++ spec/songkick/transport/curb_spec.rb
@@ -6,24 +6,20 @@ module Songkick
     describe Curb do
       after { Songkick::Transport::Curb.clear_thread_connection }
 
-      subject{ Curb.new('localhost', :connection => @fake_curl) }
-      let(:request){ Request.new('http://localhost', 'get', '/', {}) }
+      subject { described_class.new('localhost', :connection => curl) }
+      let(:request) { Request.new('http://localhost', 'get', '/', {}) }
 
       def self.it_should_raise(exception)
         it "should raise error #{exception}" do
-          begin
-            subject.execute_request(request)
-          rescue => e
-            expect(e.class).to eq(exception)
-          end
+          expect { subject.execute_request(request) }.to raise_error(exception)
         end
       end
 
       def self.when_request_raises_the_exception(raised_exception, &block)
         describe "when request raises a #{raised_exception}" do
-          before(:each) do
-            @fake_curl = FakeCurl.new(:error => raised_exception)
-          end
+          let(:curl) { instance_double(Curl::Easy, :headers => {}).as_null_object }
+
+          before { allow(curl).to receive(:http).and_raise(raised_exception) }
 
           class_exec(&block)
         end
