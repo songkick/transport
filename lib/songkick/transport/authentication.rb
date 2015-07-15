a69e3e16eb780cdebbe26173b4ad322c665b2a9a
paolo2299 <pdlawson1@gmail.com>
2015-07-15 10:15:28 +0100

    WIP

--- lib/songkick/transport/authentication.rb
+++ lib/songkick/transport/authentication.rb
@@ -9,8 +9,13 @@ module Songkick
       def basic_auth_headers(credentials)
         username = credentials.fetch(:username)
         password = credentials.fetch(:password)
-        encoded_creds = Base64.strict_encode64("#{username}:#{password}")
+        encoded_creds = strict_encode64("#{username}:#{password}")
         Headers.new({"Authorization" => "Basic #{encoded_creds}"})
+      end
+
+      # Base64.strict_encode64 is not available on Ruby 1.8.7
+      def strict_encode64(str)
+        Base64.encode64(str).gsub("\n", '')
       end
     
     end
